0285ec9f191159a7650a4a0743d58be968278900
Callum <callumtaitmate@gmail.com>
2023-04-10 23:28:01 +0100

    added project iterator key

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,7 +14,7 @@ function Projects({}: Props) {
     transition={{ duration: 2}}
     className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-0">
         {projects.map((project, i) => (
-          <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+          <div key={i} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
             <motion.img 
             initial={{
               y: -300,
@@ -26,7 +26,7 @@ function Projects({}: Props) {
             src="https://res.cloudinary.com/dj2fwrhvv/image/upload/v1681076905/imageedit_1_3539788253_vbw3ax.png"/>
             <div className="space-y-10 px-0 md:px-10 max-w-6xl">
               <h4 className="text-4xl font-semibold text-center">
-                <span className="underline decoration-[#1cb3ff7e]/50">
+                <span  className="underline decoration-[#1cb3ff7e]/50">
                   Case Study {i + 1} of {projects.length}:
                 </span>{" "}
                 UPS Clone
