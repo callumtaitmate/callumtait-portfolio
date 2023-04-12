05fec1d1c6bb405ecb03fa85c1c3eab3e82f4e3d
Callum <callumtaitmate@gmail.com>
2023-04-12 03:08:01 +0100

    sanity projectId as string

--- sanity.ts
+++ sanity.ts
@@ -1,7 +1,7 @@
 import {createClient} from 'next-sanity'
 import createImageUrlBuilder from "@sanity/image-url"
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID // "pv8y60vp"
+const projectId = "xksbbm3a" // "pv8y60vp"
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production" // "production"
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION // "2022-11-16"
 
